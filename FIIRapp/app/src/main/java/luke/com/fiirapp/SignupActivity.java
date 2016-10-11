42fbf2f1dac514f5991fb5e408ea6565ddd02323
Luke Salamone <lukesalamone94@gmail.com>
2016-10-11 03:48:59 -0500

    added signup activity

---
+++ FIIRapp/app/src/main/java/luke/com/fiirapp/SignupActivity.java
@@ -0,0 +1,17 @@
+package luke.com.fiirapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by luke on 10/11/16.
+ */
+
+public class SignupActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
